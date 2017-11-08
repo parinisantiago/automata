f818e69c107158fca953972ac59644f614dc1006
Santiago Parini <santiago.parini@globallogic.com>
2017-11-08 17:31:50 -0300

    Tiempo de delay

--- src/robot/Enviroment.java
+++ src/robot/Enviroment.java
@@ -1,5 +1,7 @@
 package robot;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by santiago.parini on 10/13/2017.
  */
@@ -8,18 +10,23 @@ public class Enviroment
 
     private static final int height = 5;
     private static final int widht = 5;
+    private static final int waitTime = 1;
 
     public static void main(String[] args)
     {
         Playground playground = new Playground(height,widht);
         Robot robot = new Robot(playground);
         playground.print(robot.getPos());
-
-        while(playground.isDirty())
+        try
         {
+            while(playground.isDirty())
+            {
             robot.clean();
             playground.print(robot.getPos());
-        }
+            TimeUnit.SECONDS.sleep(waitTime);
+            }
+        } catch (Exception e){System.out.print("Error");}
+
     }
 
 }
